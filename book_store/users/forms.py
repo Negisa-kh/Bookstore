0298bbafbc7eba9def4c4321c937dc3a9eb746ba
yaghoubi-mn <yaghoubi.mn@gmail.com>
2024-05-21 17:51:33 +0330

    add login

--- book_store/users/forms.py
+++ book_store/users/forms.py
@@ -1,16 +1,30 @@
 from typing import Any
 from django import forms
+from utils.field_validation import is_number, check_password
 
 class SignupForm(forms.Form):
     name = forms.CharField(max_length=50)
     number = forms.CharField(max_length=12)
     password = forms.CharField(max_length=50)
+    code = forms.IntegerField()
 
     def clean(self) -> dict[str, Any]:
         cd = self.cleaned_data
-        if len(cd.get("password")) < 8:
+        if check_password(cd.get("password")):
             raise ValueError("password is less than 8")
-        if len(cd.get("number")) != 11 or cd.get("number")[0:2] != "09":
+        if is_number(cd.get("number")):
             raise ValueError("invalid number")
         print("clean i'm here")
+        return super().clean()
+    
+
+class LoginForm(forms.Form):
+    number = forms.CharField(max_length=12)
+    password = forms.CharField(max_length=50)
+    code = forms.IntegerField()
+
+    def clean(self) -> dict[str, Any]:
+        cd = self.cleaned_data
+        if is_number(cd.get("number")):
+            raise ValueError("invalid number")
         return super().clean()
--- book_store/users/views.py
+++ book_store/users/views.py
@@ -1,25 +1,74 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 from .models import User
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import login as auth_login
+import random
 
 # Create your views here.
 def signup(request):
+    message = ""
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user = User.objects.get(number=form.cleaned_data.get("number"))
             if user != None:
                 form.add_error("number", ValueError("number already exist"))
+                
             else:
-                user = User()
-                user.name = form.cleaned_data.get("name")
-                user.password = make_password(form.cleaned_data.get("password"))
-                user.number = form.cleaned_data.get("number")
-                User.objects.create(user)
-                return redirect("/")
+                if form.cleaned_data.get("code") == 0:
+                    code = random.randint(100000, 999999)
+                    print("code: ", code)
+                    request.session["code"] = code
+                    # send code to number
+                    message = "code sent"
+                else:
+                    if code == request.sesison.get("code"):
+                        user = User()
+                        user.fill(form)
+                        user.name = form.cleaned_data.get("name")
+                        user.password = make_password(form.cleaned_data.get("password"))
+                        user.number = form.cleaned_data.get("number")
+                        User.objects.create(user)
+                        next = request.POST.get("next")
+                        return redirect(next)
+                    else:
+                        message = "invalid code"
 
     else:
         form = SignupForm()
     
-    return render(request, "users/signup.html", {"form": form})
+    return render(request, "users/signup.html", {"form": form, "message":message})
+
+
+def login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = User.objects.get(number=form.cleaned_data.get("number"))
+            if user != None:
+                if form.cleaned_data.get("login_mode") == "password":
+                        if user.password == form.cleaned_data.get("password"):
+                            auth_login(request, user)
+                            return redirect(request.POST.get("next"))
+
+                        else:
+                            message = "invalid number or password"
+                            
+                else:
+                    if form.cleaned_data.get("code") == 0:
+                        code = random.randint(100000, 999999)
+                        print("code: ", code)
+                        request.session["code"] = code
+                        # send code to number
+                        message = "code sent"
+                    else:
+                        if code == request.sesison.get("code"):
+
+                            auth_login(request, user)
+                            return redirect(request.POST.get("next"))
+            else:
+                form.add_error("number", "number not exist")
+    else:
+        form = LoginForm()
+    return render(request, "users/log_in.html", {"form":form, "message":message})
